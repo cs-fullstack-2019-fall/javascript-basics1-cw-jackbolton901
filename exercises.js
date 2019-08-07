882e3392f07737a7bb657511baa8e411258f6854
autumn-ragland <raglanda998@gmail.com>
2019-08-07 09:18:20 -0500

    Graded 08-07-2019 09:16:55

--- exercises.js
+++ exercises.js
@@ -4,3 +4,6 @@ alert("hello "+ nameVariable);
 var dogAge =3;
 alert("your dog is: "+ dogAge*7);
 alert("1+1= 2")
+// poblem four was an example in concatinating strings and integers
+// alert("1 + 1 = " + 2) 
+// alert("1 + 1 = " + (1+1))
